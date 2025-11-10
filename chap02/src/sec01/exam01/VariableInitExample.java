d5e924cbac01a9188233f72d756bd0de71175d6f
JaeH-Y <yjh21175753@gmail.com>
2025-11-10 17:49:35 +0900

    프로젝트 , 클래스 생성 연습/ 변수 작성 연습

---
+++ chap02/src/sec01/exam01/VariableInitExample.java
@@ -0,0 +1,15 @@
+package sec01.exam01;
+
+public class VariableInitExample {
+	
+	public static void main(String[] args) {
+		// 초기화 : 변수에 최초로 값을 저장하는 행위
+		// 초기화를 하지 않은 변수는 메모리에 저장되지 않음
+//		int value = 10;
+//		int result = value + 10;	// 컴파일 에러 발생
+		
+		int value = 30;
+		int result = value + 10;
+		System.out.println(result);
+	}
+}
