364c7a38e18cc13dfc6aa16269309000decffab0
JaeH-Y <yjh21175753@gmail.com>
2025-11-10 16:12:46 +0900

    create Java

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+/*
+도큐먼트 주석 : 도큐먼트 문서 생성하는데 사용
+*/
+// TODO주석
+// TODO: Task 뷰 추가해서 확인
+
+
+// 클래스 선언 : 클래스 이름은 대문자로 시작하고 반드시 소스 파일의 이름과 동일해야함
+public class Hello {
+	// 프로그램 실행 진입점
+	// main 메서드 : 자바 프로그램 실행 시 가장 먼저 실행되는 메서드
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
